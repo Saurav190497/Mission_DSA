f929b899db5d4ab3914714ac8d3973e93f8ebd59
saurav <sauravsrivastava786@gmail.com>
2022-06-26 10:46:04 +0530

    _03_ith_bit_finding added

---
+++ _03_ith_bit_finding.c
@@ -0,0 +1,12 @@
+// Finding the i'th bit in an user input number
+# include <stdio.h>
+int main()
+{
+    int num, pos, res;
+    printf(" Enter the number & The position at which you want to know the bit present.\n");
+    scanf("%d%d", &num, &pos);
+    printf("You had entered %d & you want to know the bit present at %d position\n", num, pos);
+    res = num & pos ;
+    printf("You had entered %d & your %d'th position bit is %d\n", num, pos, res);
+    return 0;
+}
